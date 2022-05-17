be129cd32f90e2fa1ed0a70d7ebbfe891b7b86e7
arslan <bsef19m512@pucit.edu.pk>
2022-05-17 05:54:21 +0500

    List View

--- AppendList/app/src/main/java/com/example/appendlist/MainActivity.java
+++ AppendList/app/src/main/java/com/example/appendlist/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 studentList.add(edit.getText().toString());
                 arrayAdapter.notifyDataSetChanged();
             }});
