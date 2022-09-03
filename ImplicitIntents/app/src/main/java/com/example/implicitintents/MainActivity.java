e01551616e8c0cc09db7a26644198e85ebd65423
arslanrajpot <bsef19m512@pucit.edu.pk>
2022-09-04 03:48:13 +0500

    Implicit Intent Updated

--- ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         dial=findViewById(R.id.dial);
+        
         dial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
