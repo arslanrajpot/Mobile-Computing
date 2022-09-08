500ebe87fe850ed848e6b9a461e2db78e14d66cd
arslanrajpot <bsef19m512@pucit.edu.pk>
2022-09-08 17:19:25 +0500

    Activity Life Cycle updated

--- Activity_Life_Cycle/app/src/main/java/com/example/activity_life_cycle/MainActivity.java
+++ Activity_Life_Cycle/app/src/main/java/com/example/activity_life_cycle/MainActivity.java
@@ -11,30 +11,30 @@ private static final String TAG ="MainActivity";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"OnStart");
+        Log.d(TAG,"OnStart is called");
     }
     protected void onStart(){
         super.onStart();
-        Log.d(TAG,"OnStart");
+        Log.d(TAG,"OnStart is called");
     }
     protected void onDestroy(){
         super.onDestroy();
-        Log.d(TAG,"OnDestroy");
+        Log.d(TAG,"OnDestroy is called ");
     }
     protected void onRestart(){
         super.onRestart();
-        Log.d(TAG,"OnRestart");
+        Log.d(TAG,"OnRestart is called ");
     }
     protected void onStop(){
         super.onStop();
-        Log.d(TAG,"OnStop");
+        Log.d(TAG,"OnStop is called");
     }
     protected void onPause(){
         super.onPause();
-        Log.d(TAG,"onPause");
+        Log.d(TAG,"onPause is called");
     }
     protected void onResume(){
         super.onResume();
-        Log.d(TAG,"OnStart");
+        Log.d(TAG,"OnStart is called");
     }
 }
